5e158d5cd9395a3d98526886a8cc72e4bbac51fe
tijochelappadan <tijochelappadan@gmail.com>
2020-01-28 16:50:06 +0530

    added mvc

---
+++ Controller/DoctorSearchController.cs
@@ -0,0 +1,260 @@
+﻿using MVC.Models;
+using MVC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace MVC.Controllers
+{
+    public class DoctorSearchController : Controller
+    {
+        const int RecordsPerPage = 10;
+        IDoctorSearch objIDoctorSearch;
+        IDepartmentMaster objDepartment;
+        IHospitalMaster objHospital;
+        ILocationMaster objLocation;
+        ILanguageMaster objLanguage;
+        IReferenceType objReferenceType;
+
+        public DoctorSearchController()
+        {
+            objIDoctorSearch = new DoctorSearch();
+            objDepartment = new DepartmentMaster();
+            objHospital = new HospitalMaster();
+            objLocation = new LocationMaster();
+            objLanguage = new LanguageMaster();
+            objReferenceType = new ReferenceType();
+        }
+
+        public ActionResult DoctorSearch(DoctorMasterDTOVM model, string SearchButton)
+        {
+            if (model.FromDate < Convert.ToDateTime("01 Jan 2018"))
+            {
+                model.FromDate = DateTime.Now.Date;
+                model.ToDate = DateTime.Now.Date;
+            }
+
+            List<DepartmentMasterDTO> listDepartment = new List<DepartmentMasterDTO>();
+            listDepartment = new List<DepartmentMasterDTO>()
+            {
+                new DepartmentMasterDTO { DepartmentID = 0, Department ="Select"}
+            };
+
+            foreach (var item in objDepartment.GetDepartment())
+            {
+                DepartmentMasterDTO sm = new DepartmentMasterDTO();
+                sm.DepartmentID = item.DepartmentID;
+                sm.Department = item.Department;
+
+                listDepartment.Add(sm);
+            }
+
+            model.ListDepartment = listDepartment;
+
+            List<HospitalMasterDTO> listHospital = new List<HospitalMasterDTO>();
+            listHospital = new List<HospitalMasterDTO>()
+            {
+                new HospitalMasterDTO { HospitalID = 0, Hospital ="Select"}
+            };
+
+            foreach (var item in objHospital.GetHospital())
+            {
+                HospitalMasterDTO sm = new HospitalMasterDTO();
+                sm.HospitalID = item.HospitalID;
+                sm.Hospital = item.Hospital;
+
+                listHospital.Add(sm);
+            }
+
+            model.ListHospital = listHospital;
+
+            List<LocationMasterDTO> listLocation = new List<LocationMasterDTO>();
+            listLocation = new List<LocationMasterDTO>()
+            {
+                new LocationMasterDTO { LocationID = 0, Location ="Select"}
+            };
+
+            foreach (var item in objLocation.GetLocation())
+            {
+                LocationMasterDTO sm = new LocationMasterDTO();
+                sm.LocationID = item.LocationID;
+                sm.Location = item.Location;
+
+                listLocation.Add(sm);
+            }
+
+            model.ListLocation = listLocation;
+
+            List<LanguageMasterDTO> listLanguage = new List<LanguageMasterDTO>();
+            listLanguage = new List<LanguageMasterDTO>()
+            {
+                new LanguageMasterDTO { LanguageID = 0, Language ="Select"}
+            };
+
+            foreach (var item in objLanguage.GetLanguage())
+            {
+                LanguageMasterDTO sm = new LanguageMasterDTO();
+                sm.LanguageID = item.LanguageID;
+                sm.Language = item.Language;
+
+                listLanguage.Add(sm);
+            }
+
+            model.ListLanguage = listLanguage;
+
+            List<ReferenceTypeDTO> listReferenceType = new List<ReferenceTypeDTO>();
+            listReferenceType = new List<ReferenceTypeDTO>()
+            {
+                new ReferenceTypeDTO { ReferenceTypeID = 0, ReferenceTypeName ="Select"}
+            };
+
+            foreach (var item in objReferenceType.GetReferenceType())
+            {
+                ReferenceTypeDTO sm = new ReferenceTypeDTO();
+                sm.ReferenceTypeID = item.ReferenceTypeID;
+                sm.ReferenceTypeName = item.ReferenceTypeName;
+
+                listReferenceType.Add(sm);
+            }
+
+            model.ListReferenceType = listReferenceType;
+
+            if (!string.IsNullOrEmpty(model.SearchButton))
+            {
+                var ListDoctors = objIDoctorSearch.GetDoctorSearch(Convert.ToString(model.Location), Convert.ToInt32(model.ReferenceTypeID),
+                        Convert.ToString(model.ReferenceName), Convert.ToDateTime(model.FromDate).Date, Convert.ToDateTime(model.ToDate).Date, 
+                        Convert.ToInt32(model.LanguageID));
+                var ListDoctorsCH = objIDoctorSearch.GetDoctorSearchConsultationHours(Convert.ToString(model.Location), Convert.ToInt32(model.ReferenceTypeID),
+                        Convert.ToString(model.ReferenceName), Convert.ToDateTime(model.FromDate).Date, Convert.ToDateTime(model.ToDate).Date, 
+                        Convert.ToInt32(model.LanguageID));
+                var pageIndex = model.Page ?? 1;
+                model.SearchResults = ListDoctors.ToPagedList(pageIndex, RecordsPerPage);
+                model.SearchResultsConsultHours = ListDoctorsCH;
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Search(DoctorMasterDTOVM model, string SearchButton)
+        {
+            if (model.FromDate < Convert.ToDateTime("01 Jan 2018"))
+            {
+                model.FromDate = DateTime.Now.Date;
+                model.ToDate = DateTime.Now.Date;
+            }
+
+            List<DepartmentMasterDTO> listDepartment = new List<DepartmentMasterDTO>();
+            listDepartment = new List<DepartmentMasterDTO>()
+            {
+                new DepartmentMasterDTO { DepartmentID = 0, Department ="Select"}
+            };
+
+            foreach (var item in objDepartment.GetDepartment())
+            {
+                DepartmentMasterDTO sm = new DepartmentMasterDTO();
+                sm.DepartmentID = item.DepartmentID;
+                sm.Department = item.Department;
+
+                listDepartment.Add(sm);
+            }
+
+            model.ListDepartment = listDepartment;
+
+            List<HospitalMasterDTO> listHospital = new List<HospitalMasterDTO>();
+            listHospital = new List<HospitalMasterDTO>()
+            {
+                new HospitalMasterDTO { HospitalID = 0, Hospital ="Select"}
+            };
+
+            foreach (var item in objHospital.GetHospital())
+            {
+                HospitalMasterDTO sm = new HospitalMasterDTO();
+                sm.HospitalID = item.HospitalID;
+                sm.Hospital = item.Hospital;
+
+                listHospital.Add(sm);
+            }
+
+            model.ListHospital = listHospital;
+
+            List<LocationMasterDTO> listLocation = new List<LocationMasterDTO>();
+            listLocation = new List<LocationMasterDTO>()
+            {
+                new LocationMasterDTO { LocationID = 0, Location ="Select"}
+            };
+
+            foreach (var item in objLocation.GetLocation())
+            {
+                LocationMasterDTO sm = new LocationMasterDTO();
+                sm.LocationID = item.LocationID;
+                sm.Location = item.Location;
+
+                listLocation.Add(sm);
+            }
+
+            model.ListLocation = listLocation;
+
+            List<LanguageMasterDTO> listLanguage = new List<LanguageMasterDTO>();
+            listLanguage = new List<LanguageMasterDTO>()
+            {
+                new LanguageMasterDTO { LanguageID = 0, Language ="Select"}
+            };
+
+            foreach (var item in objLanguage.GetLanguage())
+            {
+                LanguageMasterDTO sm = new LanguageMasterDTO();
+                sm.LanguageID = item.LanguageID;
+                sm.Language = item.Language;
+
+                listLanguage.Add(sm);
+            }
+
+            model.ListLanguage = listLanguage;
+
+            List<ReferenceTypeDTO> listReferenceType = new List<ReferenceTypeDTO>();
+            listReferenceType = new List<ReferenceTypeDTO>()
+            {
+                new ReferenceTypeDTO { ReferenceTypeID = 0, ReferenceTypeName ="Select"}
+            };
+
+            foreach (var item in objReferenceType.GetReferenceType())
+            {
+                ReferenceTypeDTO sm = new ReferenceTypeDTO();
+                sm.ReferenceTypeID = item.ReferenceTypeID;
+                sm.ReferenceTypeName = item.ReferenceTypeName;
+
+                listReferenceType.Add(sm);
+            }
+
+            model.ListReferenceType = listReferenceType;
+
+            if (!string.IsNullOrEmpty(model.SearchButton))
+            {
+                var ListDoctors = objIDoctorSearch.GetDoctorSearch(Convert.ToString(model.DoctorName), Convert.ToInt32(model.ReferenceTypeID),
+                        Convert.ToString(model.ReferenceName), Convert.ToDateTime(model.FromDate), Convert.ToDateTime(model.ToDate), Convert.ToInt32(model.LanguageID));
+                var ListDoctorsCH = objIDoctorSearch.GetDoctorSearchConsultationHours(Convert.ToString(model.DoctorName), Convert.ToInt32(model.ReferenceTypeID),
+                        Convert.ToString(model.ReferenceName), Convert.ToDateTime(model.FromDate), Convert.ToDateTime(model.ToDate), Convert.ToInt32(model.LanguageID));
+                var pageIndex = model.Page ?? 1;
+                model.SearchResults = ListDoctors.ToPagedList(pageIndex, RecordsPerPage);
+                model.SearchResultsConsultHours = ListDoctorsCH;
+            }
+
+            return View(model);
+        }
+
+        public JsonResult GetDatabyDoctorName(string term)
+        {
+            var list = objIDoctorSearch.DoctorAutoComplete(term);
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetReference(string term)
+        {
+            var list = objIDoctorSearch.ReferenceAutoComplete(0, term);
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Model/DoctorMasterDTO.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+using PagedList;
+
+namespace MVC.Models
+{
+    public class DoctorMasterDTO
+    {
+        [Key]
+        public int DoctorID { get; set; }
+        [Remote("DoctornameExists", "Doctor", ErrorMessage = "Doctor Name Already Exists ")]
+        [Required(ErrorMessage = "Enter Doctor Name")]
+        public string DoctorName { get; set; }
+
+        [Display(Name = "Department")]
+        [ValidateDepartment_Doctor(ErrorMessage = "Select Department")]
+        public int DepartmentID { get; set; }
+
+        [Display(Name = "Hospital")]
+        [ValidateHospital_Doctor(ErrorMessage = "Select Hospital")]
+        public int HospitalID { get; set; }
+
+        [Display(Name = "Location")]
+        [ValidateLocation_Doctor(ErrorMessage = "Select Location")]
+        public int LocationID { get; set; }
+
+        [Display(Name = "Language")]
+        [ValidateLanguage_Doctor(ErrorMessage = "Select Language")]
+        public int LanguageID { get; set; }
+
+        public string ConsultationHours { get; set; }
+        public string Department { get; set; }
+        public string Hospital { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+        public int UserID { get; set; }
+
+        public IEnumerable<DepartmentMasterDTO> ListDepartment { get; set; }
+        public IEnumerable<HospitalMasterDTO> ListHospital { get; set; }
+        public IEnumerable<LocationMasterDTO> ListLocation { get; set; }
+        public IEnumerable<LanguageMasterDTO> ListLanguage { get; set; }        
+    }
+
+    public class ValidateDepartment_Doctor : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (Convert.ToInt32(value) == 0 || Convert.ToInt32(value) == null)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class ValidateHospital_Doctor : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (Convert.ToInt32(value) == 0 || Convert.ToInt32(value) == null)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class ValidateLocation_Doctor : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (Convert.ToInt32(value) == 0 || Convert.ToInt32(value) == null)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class ValidateLanguage_Doctor : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (Convert.ToInt32(value) == 0 || Convert.ToInt32(value) == null)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class DoctorAutocompDTO
+    {
+        public string DoctorName { get; set; }
+        public int MainDoctorID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ReferenceAutoCompleteDTO
+    {
+        public string ReferenceName { get; set; }
+        public int MainReferenceID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class DoctorMasterDTOVM
+    {
+        public int? Page { get; set; }
+        public IPagedList<DoctorMasterDTO> SearchResults { get; set; }
+        public IEnumerable<DoctorMasterDTOVMConsultHours> SearchResultsConsultHours { get; set; }
+        public string SearchButton { get; set; }
+        public string DoctorSearch { get; set; }
+        public string DoctorName { get; set; }
+
+        [Display(Name = "Department")]
+        public int? DepartmentID { get; set; }
+        [Display(Name = "Hospital")]
+        public int? HospitalID { get; set; }
+        [Display(Name = "Location")]
+        public int? LocationID { get; set; }
+        [Display(Name = "Select Language")]
+
+        public int? LanguageID { get; set; }
+        public string Department { get; set; }
+        public string Hospital { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+
+        public IEnumerable<DepartmentMasterDTO> ListDepartment { get; set; }
+        public IEnumerable<HospitalMasterDTO> ListHospital { get; set; }
+        public IEnumerable<LocationMasterDTO> ListLocation { get; set; }
+        public IEnumerable<LanguageMasterDTO> ListLanguage { get; set; }
+        public IEnumerable<ReferenceTypeDTO> ListReferenceType { get; set; }
+
+        public int? ReferenceTypeID { get; set; }
+        public string ReferenceTypeName { get; set; }
+        public string ReferenceName { get; set; }
+        
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Select From Date")]
+        public DateTime FromDate { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "To Date")]
+        public DateTime ToDate { get; set; }
+    }
+
+    public class DoctorMasterDTOVMConsultHours
+    {
+        public int DoctorID { get; set; }
+        public DateTime ConsultingDate { get; set; }
+        public string ConsultingTime { get; set; }
+        public int StatusID { get; set; }
+    }
+}
